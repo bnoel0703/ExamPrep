ed8b296acccc29b37f98942e643602f776432f7d
bnoel0703 <bnoel0703@gmail.com>
2018-12-11 14:39:58 -0500

    Completed Listing 3-16

--- ExamPrepConsoleApp/Program.cs
+++ ExamPrepConsoleApp/Program.cs
@@ -17,6 +17,7 @@ using System.IO;
 using System.Xml.Serialization;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace ExamPrepConsoleApp
 {
@@ -187,33 +188,63 @@ namespace ExamPrepConsoleApp
 
         static void Main(string[] args)
         {
-            string plainText = "This is my super secret data";
-            string decryptedText;
-
-            // byte array to hold the encrypted message
-            byte[] cipherText;
-
-            // byte array to hold the key that was used for encryption
-            byte[] key;
-
-            // byte array to hold the initialization vector that was used for encryption
-            byte[] initializationVector;
-
-            // Create an Aes instance
-            // This creates a random key and initialization vector
-            EncryptWithAes(plainText, out cipherText, out key, out initializationVector);
-
-            decryptedText = DecryptWithAes(cipherText, key, initializationVector);
-
-            // Dump out our data
-            Console.WriteLine($"String to encrypt: {plainText}");
-            DumpBytes("Key: ", key);
-            DumpBytes("Initialization Vector: ", initializationVector);
-            DumpBytes("Encrypted ", cipherText);
-
-            Console.WriteLine(decryptedText);
 
             EndProgram();
+        }
+
+        private static void EncryptWithRSA() // Listing 3-16
+        {
+            string plainText = "This is my super secret data";
+            Console.WriteLine($"Plain text: {plainText}");
+
+            // RSA works on byte arrays, not strings of text
+            // This will convert our input string into bytes and back
+
+            ASCIIEncoding converter = new ASCIIEncoding();
+
+            // Convert the plain text into a byte array
+            byte[] plainBytes = converter.GetBytes(plainText);
+
+            DumpBytes("Plain bytes:", plainBytes);
+
+            byte[] encryptedBytes;
+            byte[] decryptedBytes;
+
+            // Create a new RSA to encrypt the data
+            // should be wrapped in using for production code
+            RSACryptoServiceProvider rsaEncrypt = new RSACryptoServiceProvider();
+
+            // get the keys out of the encryptor
+            string publicKey = rsaEncrypt.ToXmlString(includePrivateParameters: false);
+            Console.WriteLine($"Public key: {publicKey}");
+            string privateKey = rsaEncrypt.ToXmlString(includePrivateParameters: true);
+            Console.WriteLine($"Private key: {privateKey}");
+
+            // Now tell the encryptor to use the public key to encrypt the data
+            rsaEncrypt.FromXmlString(privateKey);
+
+            // Use the encryptor to encrypt the data. the fOAEP parameter
+            // specifies how the output is "padded" with extra bytes
+            // For maximum compatibility with receiving systems, set this as false
+            encryptedBytes = rsaEncrypt.Encrypt(plainBytes, fOAEP: false);
+
+            DumpBytes("Encrypted bytes: ", encryptedBytes);
+
+            // Now do the decode - use the private key for this
+            // We have sent someone our public key and they
+            // have used this to encrypt data that they are sending to us
+
+            // Create a new RSA to decrypt the data
+            // should be wrapped in using for production code
+            RSACryptoServiceProvider rsaDecrypt = new RSACryptoServiceProvider();
+
+            // Configure the decryptor from the XML in the private key
+            rsaDecrypt.FromXmlString(privateKey);
+
+            decryptedBytes = rsaDecrypt.Decrypt(encryptedBytes, fOAEP: false);
+
+            DumpBytes("Decrypted bytes: ", decryptedBytes);
+            Console.WriteLine($"Decrypted string: {converter.GetString(decryptedBytes)}");
         }
 
         private static string DecryptWithAes(byte[] cipherText, byte[] key, byte[] initializationVector)
