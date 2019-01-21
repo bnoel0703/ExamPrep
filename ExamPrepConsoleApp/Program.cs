404070ef649e67cb535b6006eb121a496f48f819
bnoel0703 <bnoel0703@gmail.com>
2019-01-21 13:58:40 -0500

    Completed Listing 4-1

--- ExamPrepConsoleApp/Program.cs
+++ ExamPrepConsoleApp/Program.cs
@@ -244,6 +244,23 @@ namespace ExamPrepConsoleApp
             EndProgram();
         }
 
+        private static void WriteToFile() // Listing 4-1
+        {
+            // Writing to a file
+            FileStream outputStream = new FileStream("OutputText.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            string outputMessageString = "Hello World";
+            byte[] outputMessageBytes = Encoding.UTF8.GetBytes(outputMessageString);
+            outputStream.Write(outputMessageBytes, 0, outputMessageBytes.Length);
+            outputStream.Close();
+            FileStream inputStream = new FileStream("OutputText.txt", FileMode.Open, FileAccess.Read);
+            long fileLength = inputStream.Length;
+            byte[] readBytes = new byte[fileLength];
+            inputStream.Read(readBytes, 0, (int)fileLength);
+            string readString = Encoding.UTF8.GetString(readBytes);
+            inputStream.Close();
+            Console.WriteLine($"Read message: {readString}");
+        }
+
         private static void BindToEventLog() // Listing 3-46
         {
             string categoryName = "Image Processing";
